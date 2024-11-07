e5185ae33ed2ade3c15006101da60900eb6d91fc
pattploy <pataranan@example.com>
2024-11-07 23:53:59 +0700

    Initial commit

---
+++ Assignment12/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Assignment12
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Assignment12/CarPage.xaml.cs
@@ -0,0 +1,11 @@
+using Assignment12.MVVM.ViewModels;
+namespace Assignment12;
+
+public partial class CarPage : ContentPage
+{
+	public CarPage()
+	{
+		InitializeComponent();
+        BindingContext = new CarPageViewModel();
+    }
+}
